816ff9b1bcc8c17c0fc0a8125746896e57d003f2
TahaNaeem1995 <160178535+TahaNaeem1995@users.noreply.github.com>
2024-06-25 16:04:12 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,53 @@
+// // // // let MakePizza
+// // // // MakePizza = "Your pizza is being preparing"
+// // // // console.log(MakePizza)
+// // // // function MakeKorma(){
+// // // //     return "korma"
+// // // // }
+// // // // let dish = MakeKorma()
+// // // // console.log(dish);
+
+// // // function MakePizza(){
+// // //     return "your pizza is  being preparing"
+// // // }
+
+// // // let MakeFries = () => "your pizza is  being preparing"
+// // // console.log(MakeFries(), "Table 1")
+
+
+// // // let SirZiaPromises = mew Promises( () => "");
+
+// // function MakePizza(){
+// //     console.log("your pizza is  being preparing");
+// //     return MakePizza
+
+// // }
+// //    let MakePizza = () => {
+// //        return "your pizza is  being preparing"
+// //    }
+
+// //    let myorder
+
+// // const helloPromise = new Promise
+// // ((resolve) => {
+// //     setTimeout(()=> {
+// //         resolve(" Hello,World!")
+// // }, 10000 // After 10 seconds
+// // })
+
+// // helloPromise.then((message) =>
+// //     console.log(message)
+// //)
+
+
+// // reject based on a condition
+// const conditionPromise = new Promise
+//     ((resolve,reject) => {
+//      const sucess = Math.random() > 0.5;
+//      //randomly choose success or failure
+//      if  (sucesss) {
+//      (parameter) reject: (reason?:any) => void
+// }
+// reject(new Error("Failure"));
+//     }
+// });
